cda520b49d78e85aa7cea7c991cdd6ea233cf114
blomster24 <miguel_florez_@hotmail.com>
2023-12-19 18:14:52 -0500

    MapStruct

---
+++ src/main/java/com/platzi/market/domain/Category.java
@@ -0,0 +1,15 @@
+package com.platzi.market.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Category {
+
+    private int categoryId;
+    private String category;
+    private boolean active;
+
+
+}
